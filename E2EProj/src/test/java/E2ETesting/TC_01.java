c12ada814d00ce6a33c3a8c727ad1b15d1c065d2
yatam <yatam@10.0.0.126>
2017-07-05 23:27:23 -0700

    modified

--- E2EProj/src/test/java/E2ETesting/TC_01.java
+++ E2EProj/src/test/java/E2ETesting/TC_01.java
@@ -6,6 +6,7 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
@@ -14,11 +15,11 @@ public class TC_01 {
 	@Test
 	public void TC_01()
 	{
-		//System.setProperty("webdriver.chrome.driver", "./Driver/chromedriver.exe");
-		//WebDriver driver = new ChromeDriver();
+		System.setProperty("webdriver.chrome.driver", "./Driver/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
 		
-		System.setProperty("webdriver.gecko.driver", "./Driver/geckodriver.exe");
-		WebDriver driver = new FirefoxDriver();
+		//System.setProperty("webdriver.gecko.driver", "./Driver/geckodriver.exe");
+		//WebDriver driver = new FirefoxDriver();
 		driver.get("https://accounts.google.com/SignUp?hl=en-GB");
 		driver.findElement(By.xpath(".//*[@id='wrapper']/div[2]/div/div[1]/p/a")).click();
 		//System.out.println(driver.getTitle());
